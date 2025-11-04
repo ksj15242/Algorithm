391a6a05a5a189fb23282d61807bf7b149a6c400
Sejin Kim <ksj15242@gmail.com>
2025-11-04 15:50:11 +0900

    [level 2] Title: 짝지어 제거하기, Time: 107.93 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,14 @@
+def solution(s):
+    stack = []
+    
+    for c in s:
+        if not stack:
+            stack.append(c)
+            continue
+            
+        if stack[-1]==c:
+            stack.pop()
+        else:
+            stack.append(c)
+            
+    return 0 if stack else 1
