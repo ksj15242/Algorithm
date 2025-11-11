9d0f1d3d9e678deb9b49d0b84e7512c4f8dce19a
Sejin Kim <ksj15242@gmail.com>
2025-11-11 11:33:22 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 9.24 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -0,0 +1,21 @@
+def solution(progresses, speeds):
+    
+    availableDays = []
+    for progress, speed in zip(progresses, speeds):
+        day = ((100-progress)+(speed-1))//speed
+        availableDays.append(day)
+    
+    answer = []
+    past = availableDays[0]
+    count = 0
+    for day in availableDays:
+        if past>=day:
+            count += 1
+        else:
+            answer.append(count)
+            past = day
+            count = 1
+    
+    answer.append(count)
+    
+    return answer
